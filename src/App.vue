a13c66702849d29eaec30b9f3583a147c8fb4df9
GI Quentin <gi-quentin@gis-macbook-pro.home>
2021-05-26 10:31:22 +0100

    Setup vue 3 cli

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Pomodoro></Pomodoro>
+  </div>
+</template>
+
+<script>
+import Pomodoro from './components/Pomodoro.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Pomodoro
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
